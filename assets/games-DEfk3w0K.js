7a162bbf2d7d686e53c7463246fe8b25d5681430
CheonKiO <CheonKiO@users.noreply.github.com>
2026-08-16 05:34:06 +0000

    deploy: Merge pull request #12 from CheonKiO/design/visual-system
    design: 반상(盤上) 디자인 시스템 전면 리디자인 CheonKiO/omok-front@7c9067386eabb44354caa1e3aa666f4ffc5bcf2e

--- assets/games-DEfk3w0K.js
+++
@@ -1,1 +0,0 @@
-import{G as e,H as a}from"./index-CFXzpZ2l.js";function r(){return a().BASEURL}function n(){return e.get(`${r()}/api/games`)}function o(t){return e.get(`${r()}/api/games/${t}`)}export{n as f,o as g};
